b6c27b4149ad8738c3428b8b3dca43a1e23132e8
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-17 19:55:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ end
 
 def printer(speakers)
   speakers.each do |name, index|
-  batch_badge_creator << #{name}
-  assign_rooms << #{index}
+  batch_badge_creator(name)
+  assign_rooms(index)
 end
 end
 
