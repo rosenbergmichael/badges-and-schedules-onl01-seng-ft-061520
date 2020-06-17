4de28f1a78a2f9eb71f66cf70cf7fc31c360abba
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-17 19:02:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+new_array=[]
+speakers.each do |name|
+new_array << "Hello, my name is #{name}."
+end
+new_array
+end
+
+def assign_rooms(speakers)
+new_array=[]
+speakers.each do |name|
+new_array << "Hello, #{name}!  You'll be assigned to room #{index}!"  
+end
+new_array
+end
